a0ecced67d1a77fa91c24cf1c52a906149f85509
Chance Rutledge <chancerutledge2@gmail.com>
2020-02-20 16:30:26 -0800

    Products refactrored to Context API

--- src/App.js
+++ src/App.js
@@ -7,20 +7,26 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//context
+import { ProductContext } from './contexts/ProductContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
+		setCart([...cart, item]);
+		
 		// add the given item to the cart
 	};
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{ products, addItem}}>	
 			<Navigation cart={cart} />
 
 			{/* Routes */}
-			<Route
+			{/* <Route
 				exact
 				path="/"
 				render={() => (
@@ -29,12 +35,14 @@ function App() {
 						addItem={addItem}
 					/>
 				)}
-			/>
+			/> */}
+			<Route exact path="/" component={Products}/>
 
 			<Route
 				path="/cart"
 				render={() => <ShoppingCart cart={cart} />}
 			/>
+			</ProductContext.Provider>
 		</div>
 	);
 }
